43e4a4e7a81cc1825372c836138b8c44ebf733af
Aysegulozen <150865343+Aysegulozen@users.noreply.github.com>
2024-04-05 21:06:04 +0100

    update link

--- my-portfolio/src/components/Navbar/index.jsx
+++ my-portfolio/src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 font-protest dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="https://aysegulozenportfolio.netlify.app/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={Logo} className="h-12" alt="Portfolio Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">A. Özen</span>
         </a>
