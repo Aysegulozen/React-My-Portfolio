692148dc879e8cb199816f0be950e49400ad32d3
Aysegulozen <150865343+Aysegulozen@users.noreply.github.com>
2024-04-05 20:41:45 +0100

    vite fixed

--- my-portfolio/src/components/Contact/index.jsx
+++ my-portfolio/src/components/Contact/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 import { Card, Button } from 'flowbite-react';
 import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
 import { FiPhone, FiMail, FiMapPin } from 'react-icons/fi';
